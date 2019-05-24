7e581b5930f858f9a9c690d41cc8ae99b195ed74
raihlima <38303006+raihlima@users.noreply.github.com>
2019-05-23 21:07:11 -0300

    v 2

---
+++ Trabalho2/app/src/main/java/com/example/trabalho2/CriarNovaTarefaActivity.java
@@ -0,0 +1,62 @@
+package com.example.trabalho2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+public class CriarNovaTarefaActivity extends AppCompatActivity {
+
+    private EditText titulo;
+    private EditText descricao;
+    private EditText dataLimite;
+    private RatingBar dificuldade;
+    private RadioGroup estado;
+    private Button criarTarefa;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_criar_nova_tarefa);
+
+        titulo = (EditText) findViewById(R.id.tituloTxtNovaTarefa);
+        descricao = (EditText) findViewById(R.id.descricaoTxtNovaTarefa);
+        dataLimite = (EditText) findViewById(R.id.dataLimteTxtNovaTarefa);
+        dificuldade = (RatingBar) findViewById(R.id.ratingBar);
+        estado = (RadioGroup) findViewById(R.id.estadoRadioGroup);
+        criarTarefa = (Button) findViewById(R.id.confirmarNovaTarefaButton);
+
+        criarTarefa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (verificaPreenchimento()) {
+                    Intent intent = new Intent();
+                    Bundle bundle = new Bundle();
+                    intent.putExtra("info", bundle);
+
+                    setResult(Activity.RESULT_OK, intent);
+                    finish();
+                } else {
+                    Toast.makeText(CriarNovaTarefaActivity.this, "Preencha todos os dados!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public boolean verificaPreenchimento(){
+        if(titulo.getText().toString().equals("")){
+            return false;
+        } else if(descricao.getText().toString().equals("")){
+            return false;
+        } else if(dataLimite.getText().toString().equals("")){
+            return false;
+        } return true;
+    }
+}
--- Trabalho2/app/src/main/java/com/example/trabalho2/MainActivity.java
+++ Trabalho2/app/src/main/java/com/example/trabalho2/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.trabalho2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button criarTarefa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        criarTarefa = (Button) findViewById(R.id.novaTarefaButton);
+
+        criarTarefa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CriarNovaTarefaActivity.class);
+                startActivityForResult(intent,1);
+            }
+        });
     }
 }
