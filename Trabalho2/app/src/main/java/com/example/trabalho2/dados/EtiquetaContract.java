4b20932708c5dda457804d9139db0b0f8183d9fd
raihlima <38303006+raihlima@users.noreply.github.com>
2019-05-31 14:23:02 -0300

    v 7.2
    Criação da tabela de etiqueta

--- Trabalho2/app/src/main/java/com/example/trabalho2/dados/EtiquetaContract.java
+++ Trabalho2/app/src/main/java/com/example/trabalho2/dados/EtiquetaContract.java
@@ -1,4 +1,17 @@
 package com.example.trabalho2.dados;
 
+import android.provider.BaseColumns;
+
 public class EtiquetaContract {
+    public final String TABELA_ETIQUETA[]={
+            EtiquetaDados._ID,
+            EtiquetaDados.COLUMN_TITULO
+    };
+
+    public static final class EtiquetaDados implements BaseColumns {
+        public static final String TABLE_NAME = "etiqueta";
+        public static final String COLUMN_TITULO = "nome";
+        public static final String CREATE_TABLE = String.format("CREATE TABLE %s (%s INTEGER PRIMARY KEY AUTOINCREMENT, %s TEXT)", TABLE_NAME, _ID, COLUMN_TITULO);
+        public static final String DROP_TABLE = String.format("DROP TABLE %s", TABLE_NAME);
+    }
 }
